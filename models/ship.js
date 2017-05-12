cdeda929c86564282ff501314b513880fcb47183
David Dye <david.dye@cfacorp.com>
2017-05-11 20:08:26 -0400

    changed out character file to ship file and created ship traits

--- models/ship.js
+++ models/ship.js
@@ -7,23 +7,25 @@ var Sequelize = require("sequelize");
 var sequelize = require("../config/connection.js");
 
 // Creates a "Character" model that matches up with DB
-var Character = sequelize.define("character", {
+var Ship = sequelize.define("ship", {
   // the routeName gets saved as a string
   routeName: Sequelize.STRING,
   // the name of the character (a string)
-  name: Sequelize.STRING,
+  shipname: Sequelize.STRING,
   // the character's role (a string)
-  role: Sequelize.STRING,
+  health: Sequelize.INTEGER,
   // the character's age (a string)
-  age: Sequelize.INTEGER,
+  railgun: Sequelize.INTEGER,
   // and the character's force points (an int)
-  forcePoints: Sequelize.INTEGER
+  scanner: Sequelize.INTEGER,
+  // and the character's force points (an int)
+  crew: Sequelize.INTEGER
 }, {
   timestamps: false
 });
 
 // Syncs with DB
-Character.sync();
+Ship.sync();
 
 // Makes the Character Model available for other files (will also create a table)
-module.exports = Character;
+module.exports = Ship;
